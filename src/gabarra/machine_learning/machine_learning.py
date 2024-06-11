131ba2182f91904b659523fca6f1843f759cb8a2
KarenFaS <karenfajardo.sa@gmail.com>
2024-06-11 13:33:10 +0200

    FEATURES Metrics funtion

--- src/gabarra/machine_learning/machine_learning.py
+++ src/gabarra/machine_learning/machine_learning.py
@@ -41,3 +41,37 @@ def linear_regression(df, target_column):
 # Example usage
 # result = linear_regression(df, 'target_column_name')
 # print(result["mean_squared_error"], result["r2_score"])
+
+
+
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+
+def calculate_metrics(y_test, predictions, model_name, decimal_places=2):
+    """
+    Calculates and returns a DataFrame with regression metrics: MAE, MAPE, MSE, and RMSE.
+
+    Parameters:
+    y_test (array-like): True values.
+    predictions (array-like): Predicted values.
+    model_name (str): Name of the model.
+    decimal_places (int, optional): Number of decimal places to display. Default is 2.
+
+    Returns:
+    pd.DataFrame: DataFrame containing the calculated metrics.
+    """
+    # Calculate metrics
+    mae = metrics.mean_absolute_error(y_test, predictions)
+    mape = metrics.mean_absolute_percentage_error(y_test, predictions)
+    mse = metrics.mean_squared_error(y_test, predictions)
+    rmse = np.sqrt(mse)
+
+    # Create a DataFrame with the metrics
+    data = {"Modelo": [model_name], 'MAE': [mae], 'MAPE': [mape], 'MSE': [mse], 'RMSE': [rmse]}
+    df_metrics = pd.DataFrame(data)
+
+    # Format the DataFrame to show specified decimal places
+    pd.options.display.float_format = f'{{:.{decimal_places}f}}'.format
+
+    return df_metrics
