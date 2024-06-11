25fbaf326911ccda4b38bf6ddf4b5b0bfd9cd3d6
adriani94bil <adrian.ruiz.donado@gmail.com>
2024-06-11 16:12:20 +0200

    Correction V1

--- tests/test_ML_linear_regression.py
+++ tests/test_ML_linear_regression.py
@@ -5,9 +5,7 @@ from sklearn.datasets import make_regression
 from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-
-# Import the function to test
-from  src import linear_regression
+from src.gabarra.machine_learning.machine_learning import *
 
 def test_linear_regression():
     # Create a dummy dataset for testing
